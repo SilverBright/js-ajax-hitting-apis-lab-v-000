3ceb044fc4c722917b0f398849eaf08829f91db0
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-04 20:15:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -45,3 +45,10 @@ function displayCommits(){
   .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList
 }
+
+function displayBranches(){
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches.map(branch => '<li>'+branch.name+ </li>')
+    .join('')}</ul>`;
+    document.getElementById('details').innerHTML = branchesList     
+}
