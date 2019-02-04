afcefb4e837bbc547cd2bcefaec39d9eadc7ab40
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-04 20:20:49 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -31,18 +31,16 @@ function displayRepositories(){
   const repos = JSON.parse(this.responseText);
   console.log(repos)
   const repoList = `<ul>${repos.map(
-                                      r =>
-                                          '<li>' +
-                                          //add repo name
-                                          r.name +
-                                          '<br> https://github.com/'+ r.owner.login + '/' + r.name +
-                                      		'<br><a href="#" data-username="'+ r.owner.login + '"data-repository="' + r.name +'"onclick="getCommits(this)">GetCommits</a>' +
-                                          //above code passes username and repository into a dataset this to be used in the function
-                                          '<br><a href="#" data-username="'+ r.owner.login + '"data-repository="' + r.name +'"onclick="getBranches(this)">GetBranches</a></li>'
-		                                 )
-                      .join('')}</ul>`;
+    r =>
+      '<li>' +
+      r.name +
+      '<br> https://github.com/'+ r.owner.login + '/' + r.name +
+      '<br><a href="#" data-username="'+ r.owner.login + '"data-repository="' + r.name +'"onclick="getCommits(this)">GetCommits</a>' +
+      '<br><a href="#" data-username="'+ r.owner.login + '"data-repository="' + r.name +'"onclick="getBranches(this)">GetBranches</a></li>')
+      .join('')}</ul>`;
     document.getElementById('repositories').innerHTML = repoList;
 }
+
 function displayCommits(){
     const commits = JSON.parse(this.responseText);
     const commitsList = `<ul>${commits.map(commit => '<li><strong>'+commit.commit.author.name+' ('+commit.author.login+')</strong>'+commit.commit.message+'</li>')
