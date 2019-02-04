fd6a953c94f80158494ef75e2a0005aba9bd7ccf
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-04 20:21:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -42,15 +42,15 @@ function displayRepositories(){
 }
 
 function displayCommits(){
-    const commits = JSON.parse(this.responseText);
-    const commitsList = `<ul>${commits.map(commit => '<li><strong>'+commit.commit.author.name+' ('+commit.author.login+')</strong>'+commit.commit.message+'</li>')
-                        .join('')}</ul>`;
-    document.getElementById('details').innerHTML = commitsList
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>'+commit.commit.author.name+' ('+commit.author.login+')</strong>'+commit.commit.message+'</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList
 }
 
 function displayBranches(){
-    const branches = JSON.parse(this.responseText);
-    const branchesList = `<ul>${branches.map(branch => '<li>'+branch.name+'</li>')
-                          .join('')}</ul>`;
-    document.getElementById('details').innerHTML = branchesList     
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches.map(branch => '<li>'+branch.name+'</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList     
 }
