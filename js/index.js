5d57f7fca71c1e985ec2489c65fd89d05338f487
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-04 20:05:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ function getRepositories() {
   let username = document.getElementById('username').value
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories)
-  req.open('GET', `https://api.github.com/users/${username}/repos`)
+  req.open('GET', 'https://api.github.com/users/'+ username +'/repos');
   req.send();
 }
